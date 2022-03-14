43ace7356c6e6ade9318c752484a89cf05d6c8cf
Ale Gomez <alejandro.gomez969@gmail.com>
2022-03-14 13:00:19 -0300

    Creacion de logica de carrito de compras

--- Desktop/Angular Prueba/primerApp/src/app/app-routing.module.ts
+++ Desktop/Angular Prueba/primerApp/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 //Si queremos redirigir a la pagina principal en caso de que la ruta no exista entonces en path colocamos ** lo que hara sera atrapar todas las rutas, en redirectTo no colocamos nada lo cual nos va a dirigir a la pagina principal, pero hay que tener cuidado ya que esta linea que atrapa todas las rutas hay que ponerla al final de todo ya que los path se ejecutan en orden si no siempre nos va a redirigir a la pagina principal cualquiera sea la ruta que coloquemos
 const routes: Routes = [
   { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
+  { path: 'checkout', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule) },
   {path: '**', redirectTo: '', pathMatch:'full'}
 ];
 
