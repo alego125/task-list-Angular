26383274f0ce2504c566f277e69676bf3fc13917
Alejandro Gomez <76167482+alego125@users.noreply.github.com>
2022-03-21 11:33:32 -0300

    Delete Desktop/task_list directory

--- Desktop/task_list/src/app/task.ts
+++
@@ -1,7 +0,0 @@
-//Para indicar las restricciones del Json lo que hacemos es asignar cada atributo dentro de una interface y colocarle el tipo que se va a usar o sea le estamos restringiendo el tipo de datos que usara desde la interface
-export interface Task{
-    id?:number; //Puede que venga con id o no si es que ya se guardo si no se guardo no tendremos un id todavia generado
-    text:string;
-    day:string;
-    reminder:boolean;
-}
